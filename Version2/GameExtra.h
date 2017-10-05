caa74d5ab01620df80e46549c745821157f64754
Christos-Nikolaos Mitsopoulos <mitsopouloschris@gmail.com>
2017-10-05 16:04:00 -0400

    Update GameExtra.h

--- Version2/GameExtra.h
+++ Version2/GameExtra.h
@@ -1,10 +1,4 @@
-/*******************************
-* COMP 11 Spring 2017
-* Homework 6
-* Christos-Nikolaos Mitsopoulos
-* Signoff by [Laney Strange] [04/11/2017]
-* EXTRA CREDIT
-********************************/
+
 
 #include <iostream>
 #include <fstream>
